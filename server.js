72f739361a8c7114dfa13ecc77c1df71e5dc50a5
youbreakyoubuycollection-bit <youbreakyoubuycollection@gmail.com>
2025-12-14 02:02:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.redirect("/widget.html");
 });
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
