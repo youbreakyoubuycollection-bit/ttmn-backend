984dfb33d8c678a806d02fdd63339728cab2d70f
youbreakyoubuycollection-bit <youbreakyoubuycollection@gmail.com>
2025-12-14 01:23:46 -0500

    Serve index.html on root route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
